5b3f6311273d08171c2cacac0f080831e8535324
Arman Doğru <arman.dogru@gmail.com>
2021-07-13 17:03:58 +0300

    Cleaned up Controller Class for readability and added TwitterRun

---
+++ src/main/java/com/getir/twitterdemo/config/TwitterRun.java
@@ -0,0 +1,24 @@
+package com.getir.twitterdemo.config;
+
+import lombok.Builder;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Builder
+public class TwitterRun {
+
+    public static String execute(String arg, int WOEID){
+        try {
+            InputStream input = new FileInputStream("src/main/resources/application.properties");
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("src/main/resources/application.properties"));
+            TwitterEngine twitter = new TwitterEngine(prop.getProperty("consumerKey"), prop.getProperty("consumerSecret"), prop.getProperty("accessToken"), prop.getProperty("tokenSecret"));
+            TrendData trendData = twitter.searchTrend(arg, WOEID);
+            return "Topic searched: " + trendData.getTrendName() + " || Currently trending: " + trendData.isTrending() + " || Tweet volume of the Trend: " + trendData.getTweetVolume();
+        } catch (Exception e) {
+            return "There was a problem with API Keys";
+        }
+    }
+}
--- src/main/java/com/getir/twitterdemo/contoller/TwitterController.java
+++ src/main/java/com/getir/twitterdemo/contoller/TwitterController.java
@@ -1,34 +1,18 @@
 package com.getir.twitterdemo.contoller;
 
-
-import com.getir.twitterdemo.config.TwitterEngine;
-import com.getir.twitterdemo.config.TrendData;
+import com.getir.twitterdemo.config.TwitterRun;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import twitter4j.TwitterException;
-
-import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.InputStream;
 
 @RestController
 @RequestMapping("twitter")
 public class TwitterController {
 
     @GetMapping("/{arg},{WOEID}")
-    public String getSegmentByCity(@PathVariable String arg, int WOEID) throws TwitterException, IllegalArgumentException {
+    public String getSegmentByCity(@PathVariable String arg, int WOEID) {
         //localhost:8080/twitter/{getir},{23424969}
-        try {
-            InputStream input = new FileInputStream("src/main/resources/application.properties");
-            Properties prop = new Properties();
-            prop.load(new FileInputStream("src/main/resources/application.properties"));
-            TwitterEngine twitter = new TwitterEngine(prop.getProperty("consumerKey"), prop.getProperty("consumerSecret"), prop.getProperty("accessToken"), prop.getProperty("tokenSecret"));
-            TrendData trendData = twitter.searchTrend(arg, WOEID);
-            return "Topic searched: " + trendData.getTrendName() + " || Currently trending: " + trendData.isTrending() + " || Tweet volume of the Trend: " + trendData.getTweetVolume();
-        } catch (Exception e) {
-            return "There was a problem with API Keys";
-        }
+        return TwitterRun.execute(arg,WOEID);
     }
 }
