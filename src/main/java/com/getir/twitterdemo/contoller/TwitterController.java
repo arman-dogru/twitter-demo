f4ac175dbd55e52301634cbcaa3c7d099203d9c0
Arman Doğru <arman.dogru@gmail.com>
2021-07-13 15:44:49 +0300

    Handled exceptions in Controller

--- src/main/java/com/getir/twitterdemo/contoller/TwitterController.java
+++ src/main/java/com/getir/twitterdemo/contoller/TwitterController.java
@@ -2,7 +2,7 @@ package com.getir.twitterdemo.contoller;
 
 import com.getir.twitterdemo.config.Login;
 import com.getir.twitterdemo.config.TwitterEngine;
-import com.getir.twitterdemo.model.TrendData;
+import com.getir.twitterdemo.config.TrendData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +17,15 @@ public class TwitterController {
     @GetMapping("/{arg},{WOEID}")
     public String getSegmentByCity(@PathVariable String arg, int WOEID) throws TwitterException, IllegalArgumentException {
         //localhost:8080/twitter/{getir},{23424969}
-        TwitterEngine twitter = new TwitterEngine(Login.getConsumerKey(),Login.getConsumerSecret(),Login.getAccessToken(),Login.getTokenSecret());
+        try {
+            TwitterEngine twitter = new TwitterEngine(Login.getConsumerKey(), Login.getConsumerSecret(), Login.getAccessToken(), Login.getTokenSecret());
+        } catch (Exception e) {
+            return "There was a problem with API Keys";
+        }
+
+        TwitterEngine twitter = new TwitterEngine(Login.getConsumerKey(), Login.getConsumerSecret(), Login.getAccessToken(), Login.getTokenSecret());
         TrendData trendData = twitter.searchTrend(arg, WOEID);
-        return "Topic searched: "+trendData.getTrendName()+" || Currently trending: "+trendData.isTrending()+" || Tweet volume of the Trend: "+trendData.getTweetVolume();
+        return "Topic searched: " + trendData.getTrendName() + " || Currently trending: " + trendData.isTrending() + " || Tweet volume of the Trend: " + trendData.getTweetVolume();
     }
 
 }
