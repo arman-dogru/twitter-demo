16c9ae80c3f69aecd8d4639579131adbdee2d931
Arman Doğru <arman.dogru@gmail.com>
2021-07-13 11:26:07 +0300

    Adjusted variables for easier use

--- src/main/java/com/getir/twitterdemo/contoller/TwitterController.java
+++ src/main/java/com/getir/twitterdemo/contoller/TwitterController.java
@@ -14,14 +14,11 @@ import twitter4j.TwitterException;
 @RequestMapping("twitter")
 public class TwitterController {
 
-    @GetMapping("/{args}")
-    public String getSegmentByCity(@PathVariable String[] args) throws TwitterException, IllegalArgumentException {
+    @GetMapping("/{arg},{WOEID}")
+    public String getSegmentByCity(@PathVariable String arg, int WOEID) throws TwitterException, IllegalArgumentException {
         //localhost:8080/twitter/{getir},{23424969}
-        String arg = args[0];
-        int WOEID = Integer.parseInt(args[1]);
         TwitterEngine twitter = new TwitterEngine(Login.getConsumerKey(),Login.getConsumerSecret(),Login.getAccessToken(),Login.getTokenSecret());
         TrendData trendData = twitter.searchTrend(arg, WOEID);
-
         return "Topic searched: "+trendData.getTrendName()+" || Currently trending: "+trendData.isTrending()+" || Tweet volume of the Trend: "+trendData.getTweetVolume();
     }
 
